f38894584f22e69fb369ceb243466fe7e844d20f
Marcelo Fuentes <marcelofuentes_10@hotmail.com>
2026-04-26 20:43:06 -0600

    Diseño de pantallas, componentes y rutas

---
+++ src/heroes/pages/search/SearchPage.tsx
@@ -0,0 +1,14 @@
+import { CustomJumbotron } from "@/components/custom/CustomJumbotron";
+
+export const SearchPage = () => {
+  return (
+    <>
+      <CustomJumbotron
+        title="Búsqueda de Super Héroes"
+        description="Descubre, explora y administra super héroes y villanos"
+      />
+    </>
+  );
+};
+
+export default SearchPage;
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+
+import { HeroesApp } from "./HeroesApp";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <h1>hola mundo</h1>
+    <HeroesApp />
   </StrictMode>,
 );
